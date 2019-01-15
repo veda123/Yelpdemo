b59eddecb5ba22db1a9190be7ef1838653ffde42
Veda Ramaiah <Veda.RAMAIAH@us.thalesgroup.com>
2019-01-15 16:49:11 -0500

    Initail commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :restaurants
+  get 'pages/about' => 'pages#about'
+  get 'pages/contact'  => 'pages#contact'
+
+  root 'restaurants#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
