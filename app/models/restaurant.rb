c25fee9e8837afbd6ea55ee2fc7cd9c9e8745313
Veda Ramaiah <Veda.RAMAIAH@us.thalesgroup.com>
2019-01-18 17:40:12 -0500

    included bootstrap and image field

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+      validates :name, :address, :phone, :website, presence: true 
+      mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_202954) do
+ActiveRecord::Schema.define(version: 2019_01_16_214830) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_01_15_202954) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
